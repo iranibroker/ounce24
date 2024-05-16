52783afc2fae3231eb34275e3dccf823e5f14907
Mahdi Ketabdar <mahdi7192@gmail.com>
2024-05-16 16:30:07 +0330

    error handler

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,6 +14,18 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
+
+  // Process event handlers
+  process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err);
+    // Handle the error gracefully, log it, and take appropriate actions
+  });
+
+  process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+    // Handle the rejection gracefully, log it, and take appropriate actions
+  });
+
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
